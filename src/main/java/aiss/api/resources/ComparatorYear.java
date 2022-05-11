6aa799bab831d5be152ff5a38ef8dd4f2c4f4357
María Vico <73558808+marvicmar@users.noreply.github.com>
2022-05-11 12:33:29 +0200

    update comparatorYear

--- src/main/java/aiss/api/resources/ComparatorYear.java
+++ src/main/java/aiss/api/resources/ComparatorYear.java
@@ -2,12 +2,12 @@ package aiss.api.resources;
 
 import java.util.Comparator;
 
-import aiss.model.Song;
+import aiss.model.Book;
 
-public class ComparatorYear implements Comparator<Song>{
+public class ComparatorYear implements Comparator<Book>{
 
 	@Override
-	public int compare(Song o1, Song o2) {
+	public int compare(Book o1, Book o2) {
 		// TODO Auto-generated method stub
 		return o1.getYear().compareTo(o2.getYear());
 	}
