43e7ce8e9eae7aa97d0d3d1c62329b67c6f17311
CarmenRuizPorcel <72879200+CarmenRuizPorcel@users.noreply.github.com>
2022-05-17 19:24:42 +0200

    Actualización user

---
+++ src/main/java/aiss/api/AiisrateApplication.java
@@ -0,0 +1,36 @@
+package aiss.api;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import aiss.api.resources.BookResource;
+import aiss.api.resources.BooklistResource;
+import aiss.api.resources.UserResource;
+
+
+
+public class AiisrateApplication extends Application {
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> classes = new HashSet<Class<?>>();
+
+	// Loads all resources that are implemented in the application
+	// so that they can be found by RESTEasy.
+	public AiisrateApplication() {
+
+		singletons.add(BooklistResource.getInstance());
+		singletons.add(BookResource.getInstance());
+		singletons.add(UserResource.getInstance());
+	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return classes;
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
