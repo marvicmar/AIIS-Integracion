ece15c41dd29821f49d36a42ff5c928ad59e0f30
sergiosant02 <54277294+sergiosant02@users.noreply.github.com>
2022-05-20 13:17:34 +0200

    Add files via upload

---
+++ User.java
@@ -0,0 +1,97 @@
+package aiss.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import aiss.model.repository.MapRepository;
+
+public class User {
+
+	private String id;
+	private String name;
+	private String userName;
+	private String email;
+	private Set<String> wishList;
+	
+	public User() {
+	}
+	
+	public User(String name, String userName, String email, Set<String> wishList) {
+		this.name = name;
+		this.userName = userName;
+		this.email = email;
+		this.wishList = wishList;
+	}
+	
+	public User(String id, String name, String userName, String email, Set<String> wishList) {
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.wishList = wishList;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Set<String> getWishList() {
+		return wishList;
+	}
+	
+	public String getWish(String id) {
+		if (wishList ==  null) {
+			return null;
+		}
+		
+		String wish = null;
+		for(String w: wishList) {
+			for(Booklist bl: MapRepository.getInstance().getAllBooklists()) {
+				if(bl.getId() == w) {
+					wish = MapRepository.getInstance().getBooklist(w).toString();
+					break;
+				}
+			}
+		}
+		return wish;
+	}
+	
+	public void addWish(String w) {
+		if (wishList == null) {
+			wishList = new HashSet<String>();
+		}
+		wishList.add(w);
+	}
+	
+	public void deleteWish(String w) {
+		wishList.remove(w);
+	}
+	
+}
