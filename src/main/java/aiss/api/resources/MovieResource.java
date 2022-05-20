08bf00ede5c92452129ebf4ec9fc108fcb2047f5
sergiosant02 <54277294+sergiosant02@users.noreply.github.com>
2022-05-20 12:29:44 +0200

    Add files via upload

--- src/main/java/aiss/api/resources/MovieResource.java
+++ src/main/java/aiss/api/resources/MovieResource.java
@@ -2,6 +2,7 @@ package aiss.api.resources;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,29 +13,73 @@ import org.restlet.resource.ClientResource;
 import org.restlet.resource.ResourceException;
 
 import aiss.model.Movie;
+import aiss.model.MovieResult;
+import aiss.model.repository.BooklistRepository;
+import aiss.model.repository.MapBooklistRepository;
 
 @Path("/movies")
 public class MovieResource {
 	
+	private static MovieResource _instance=null;
 	
+	
+	public static MovieResource getInstance()
+	{
+		if(_instance==null)
+				_instance=new MovieResource();
+		return _instance;
+	}
 	
 	private String uri = "https://api.themoviedb.org/3/movie/popular?api_key=c00b192fdcd2c592093704c75356d684";
 	
 	@GET
 	@Produces("application/json")
-	public Collection<Movie> getAll(@QueryParam("language") String language, @QueryParam("page") Integer page) {
+	public MovieResult getAll(@QueryParam("language") String language, @QueryParam("page") Integer page) {
 		ClientResource cr = null;
-		Movie [] movies = null;
+		MovieResult movies = null;
+		String urlFinal = String.format("%s&language=%s&page=%s", uri, language, page);
+		System.out.println("###################################################################");
+		System.out.println(urlFinal);
+		
 		try {
-			String urlFinal = String.format("%s&language=%s&page=%s", uri, language, page);
+			System.out.println("###################################################################movie");
 			cr = new ClientResource(urlFinal);
-			movies = cr.get(Movie[].class);
+			//System.out.println(cr.get().getText());
+			movies = cr.get(MovieResult.class);
+			System.out.println(movies);
 			
 		} catch (ResourceException re) {
 			System.err.println("Error when retrieving the collections of movies: " + cr.getResponse().getStatus());
+			System.out.println("Error: " + re);
 		}
 		
-		return Arrays.asList(movies);
+		return movies;
+	}
+	
+	@GET
+	@Path("/p")
+	@Produces("application/json")
+	public Movie get(@QueryParam("language") String language, @QueryParam("page") Integer page) {
+		ClientResource cr = null;
+		Movie movie = null;
+		String urlFinal = "https://api.themoviedb.org/3/movie/latest?api_key=c00b192fdcd2c592093704c75356d684&language=es-ES";
+		System.out.println("###################################################################sola");
+		System.out.println(urlFinal);
+		
+		try {
+			System.out.println("###################################################################movie sola");
+			cr = new ClientResource(urlFinal);
+			System.out.println("###################################################################movie sola 2");
+			System.out.println(cr);
+			movie = cr.get(Movie.class);
+			System.out.println("Resultado de movie: " + movie);
+			
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving the collections of movies: " + cr.getResponse().getStatus());
+			System.out.println("Error: " + re);
+		}
+		
+		return movie;
 	}
 }
 
