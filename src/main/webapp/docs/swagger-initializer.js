76c323ec6c7e9dba5225b73c5bb6677f1ee5b585
CarmenRuizPorcel <72879200+CarmenRuizPorcel@users.noreply.github.com>
2022-05-29 17:21:20 +0200

    Update initializer

---
+++ src/main/webapp/docs/swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://wishes-350515.oa.r.appspot.com/docs/documentacion_wishes-2.0-swagger.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
